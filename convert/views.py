4c70df3132039608c593f606d0ea58793c2988ca
KT-AIVLE-d066322 <d066322@aivle.kt.co.kr>
2024-11-14 10:30:12 +0900

    [Model] Fix import error

--- convert/views.py
+++ convert/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
-from .models import ConvertImages
 from .serializers import ConvertImagesSerializer
 
 
